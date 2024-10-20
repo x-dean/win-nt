c1c71819033beead4497ef7363af93e4f7829a61
dx <deanxaka@gmail.com>
2024-10-20 16:55:28 +0200

    [sppsvc]

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # win-nt
-This repo is dedicated to Windows
+
+### Due to sppsvc bug (or KMS stuff !!) SPPSVC service works full time using 30%+ CPU. To mitigate this issue I tried to stop the service or not start it at all during startup. This was a solution but this service is needed to keep license validated and also be able to use Microsoft Products like Office etc. So I spend some weekend hours to create this script to suspend ;) the service, so for Windows it is running but for us the 30% CPU is back.
+
+# How to use it:
+- Execute with powershell.
+- Your user need to be local administrator of the system.
+
+## How to revert it (in case you want to validate  windows or update or stuff like that):
+- Go to Task Scheduler, find "SPPSVC Suspend", right click and disable. Reboot.
+
+## To re-enable
+- Go to Task Scheduler, find "SPPSVC Suspend", right click and enable. Reboot.
+
+> Because the script is scheduled to run within 15 min sppsvc will run to do his tasks then will be suspended.
+> Tools used are downloaded from Microsoft PSTools.
---
+++ auto_suspend_sppsvc.ps1
@@ -0,0 +1,102 @@
+﻿function Test-IsAdmin {
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($identity)
+    return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (-Not (Test-IsAdmin)) {
+    Write-Host "This script requires administrative privileges. Attempting to restart as administrator...`n"
+    
+    $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    Start-Process powershell -ArgumentList $arguments -Verb RunAs
+    exit
+}
+
+$tempDir = "$env:USERPROFILE\temp"
+$zipUrl = "https://download.sysinternals.com/files/PSTools.zip"
+$zipPath = "$tempDir\PSTools.zip"
+$extractPath = "$tempDir\PSTools"
+$psSuspendPath = "$extractPath\pssuspend64.exe"
+$destinationPath = "C:\Windows\System32\pssuspend64.exe"
+
+if (-Not (Test-Path $tempDir)) {
+    New-Item -ItemType Directory -Path $tempDir
+}
+
+if (-Not (Test-Path $zipPath)) {
+    Write-Host "Downloading PSTools.zip...`n"
+    Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+} else {
+    Write-Host "PSTools.zip already exists."
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+if (-Not (Test-Path $extractPath)) {
+    try {
+        Write-Host "Extracting PSTools.zip...`n"
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $extractPath)
+    } catch {
+        Write-Host "Error extracting PSTools.zip: $_ `n"
+        exit
+    }
+} else {
+    Write-Host "PSTools already extracted."
+}
+
+
+if (-Not (Test-Path $destinationPath)) {
+    if (Test-Path $psSuspendPath) {
+        Write-Host "Copying pssuspend64.exe to System32...`n"
+        Copy-Item -Path $psSuspendPath -Destination $destinationPath -Force
+    } else {
+        Write-Host "Error: pssuspend64.exe not found at $psSuspendPath `n"
+        exit
+    }
+} else {
+    Write-Host "pssuspend64.exe already exists in System32. `n"
+}
+
+$taskName = "SPPSVC Suspend"
+
+$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\pssuspend64.exe" -Argument "sppsvc"
+$trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay 15
+
+$currentUser = [Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+$principal = New-ScheduledTaskPrincipal -UserId $currentUser -LogonType Interactive -RunLevel Highest
+
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -ExecutionTimeLimit ([TimeSpan]::Zero)
+
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -eq $taskName}
+
+if (-Not $taskExists) {
+    Write-Host "Creating scheduled task '$taskName'... `n"
+    Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings -TaskName $taskName
+    # wevtutil set-log Microsoft-Windows-TaskScheduler/Operational /enabled:true
+} else {
+    Write-Host "Scheduled task '$taskName' already exists. `n"
+}
+
+Write-Host "Running the task to suspend sppsvc service... `n"
+Start-ScheduledTask -TaskName $taskName
+
+
+if (Test-Path $tempDir) {
+    Write-Host "Cleaning up temporary files... `n"
+    Remove-Item -Path $tempDir -Recurse -Force
+}
+
+Write-Host "Checking the status of the Software Protection service (sppsvc)...`n"
+
+$threads = (Get-Process -Name sppsvc).Threads | Select-Object ThreadState, WaitReason
+
+if ($threads) {
+    Write-Host "SPPSVC threads found. Displaying ThreadState and WaitReason:`n"
+    $threads
+} else {
+    Write-Host "No threads found for the sppsvc process.`n"
+}
+
+Write-Host "Script execution completed. Press Enter to exit."
+[void][System.Console]::ReadLine()
